4d467a31e0adc1c1ce3b160ac9a2da907f0b4ff8
wysaid <this@wysaid.org>
2021-09-14 12:23:55 +0800

    solve compile issue for NDK-r20e

--- library/src/main/jni/Application.mk
+++ library/src/main/jni/Application.mk
@@ -2,12 +2,12 @@
 APP_ABI :=  all
 #APP_ABI :=  armeabi-v7a
 
-APP_PLATFORM := android-14
+APP_PLATFORM := android-16
 
-APP_STL := gnustl_static
+APP_STL := c++_static
 
 #APP_CPPFLAGS := -frtti -fexceptions
 #APP_CPPFLAGS := -fpermissive
-APP_CPPFLAGS := -frtti -std=gnu++11
+APP_CPPFLAGS := -frtti -std=c++11
 
 APP_OPTIM := release
